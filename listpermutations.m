1e0513f48e01e7b14f94c7d1ee1e41f99dd63004
clfreeman7 <39930901+clfreeman7@users.noreply.github.com>
2022-01-28 12:04:03 -0600

    Add files via upload

---
+++ listpermutations.m
@@ -0,0 +1,55 @@
+function allPerms = listpermutations(gaitCycle, issymmetric)
+% LISTPERMUTATIONS  
+% listpermutations(gaitCycle) lists all m unique cyclic and symmetric
+% permutations of the robot states in the n-vector gaitCycle as rows in an
+% m x n matrix .
+%
+% For an assymmetric robot:
+% listpermutations(gaitCycle, issymmetric) lists all m uniquen cyclic and
+% symmetric permutations of the robot states in the n-vector gaitCycle as
+% rows in an m x n matrix. Symmetric permutations will be excluded if
+% issymmetric = false.
+%
+% Caitlin Freeman, Agile Robotics Lab (UA)
+% 1/27/22
+
+
+
+    if nargin < 2
+        issymmetric = true;
+    end
+   
+    if ~isvector(gaitCycle)
+        error('gaitCycle must be a vector of robot state labels.');
+    end
+   
+    allPerms(1, :) = gaitCycle;
+    lenCycle = length(gaitCycle);
+    
+    % Find symmetric permutation states
+    if issymmetric
+        origStates= dec2bin(0:15, 4);    % actuation states as 4 bit strings
+        symStates = origStates;
+        symLabels(1, :) = 1:16;
+        for i = 1:3
+            symStates = circshift(symStates, 1, 2);
+            [~, symLabels(i+1, :)] = ismember(symStates, origStates, 'rows');
+        end
+    end
+    
+    % Find symmetric permutation cycles 
+    for i = 1:lenCycle
+        % Find cyclic permutations
+       cyclicPerms = circshift(gaitCycle, i);
+       if issymmetric
+           symPerms = symLabels(:, cyclicPerms);
+           allPerms = [allPerms; symPerms];
+       else
+           if i < lenCycle
+               allPerms = [allPerms; cyclicPerms];
+           end
+       end
+    
+    % Remove any repeated permutations
+    allPerms = unique(allPerms, 'rows');
+end
