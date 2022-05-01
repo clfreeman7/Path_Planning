874e4a708a57d170b3f82d1a74deedbf49638393
Caitlin Freeman <39930901+clfreeman7@users.noreply.github.com>
2022-05-01 17:54:44 -0500

    Add files via upload

---
+++ demos/demo_MSoRo_IO.m
@@ -0,0 +1,31 @@
+% 
+% Script: demo_MSoRo_IO.m
+%  
+% Dependencies:
+% Dependencies:
+%   +demos/data/gait_library_2.mat
+%
+%   +msoro/+api.MSoRo_IO.m
+%
+% Description: 
+%   Demonstrate how to create use experimental data for open-loop paths. 
+
+% [0] == Script setup
+clear; clc
+% Add dependencies to classpath
+addpath('../');
+% Load the gait library data. 
+load('data/gait_library_2.mat')
+
+% [1] == Extract and define gait data.
+n_gaits = length(gait_library_2);
+% Extract the motion data and gait names from the gait library
+gait_trans_seq = cell(1, n_gaits);
+gait_names = cell(1, n_gaits);
+for i = 1:n_gaits
+    gait_trans_seq{i} = gait_library_2(i).robo_states;
+    gait_names{i} = gait_library_2(i).gait_name;
+end
+
+gait_trans_seq
+gait_names
