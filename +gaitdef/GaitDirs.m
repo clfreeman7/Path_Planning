72f19ee4618a9ff11e5ebb17aa96bbf653500234
achang <alexander.h.chang@gatech.edu>
2022-05-04 14:48:11 -0400

    michael needs these -> reinstating classes. will rename appropriately to avoid confusion with Gait.m

---
+++ +gaitdef/GaitDirs.m
@@ -0,0 +1,17 @@
+% ======================= GaitDirs =======================
+%
+%  Enumeration class defining gait directions associated with MSoRo 
+%  locomotion gaits.
+%
+%  GaitDirs()
+%
+%  ====================== GaitDirs ========================
+classdef GaitDirs
+
+  enumeration
+      NE, NW, SW, SE, CW, CCW
+   end
+
+end     % class
+
+
---
+++ +gaitdef/Gaits.m
@@ -0,0 +1,16 @@
+% ======================= Gaits =======================
+%
+%  Enumeration class defining available gaits that may be selected/applied
+%  for MSoRo mobile robot.
+%
+%  Gaits()
+%
+%  ====================== Gaits ========================
+classdef Gaits
+
+  enumeration
+      TRANSLATE, ROTATE
+   end
+
+end     % class
+
