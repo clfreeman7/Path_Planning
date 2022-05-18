e90cf9d746955e5c1561ad48f8effe583135857a
arunniddish <63624213+arunniddish@users.noreply.github.com>
2022-05-17 21:38:35 -0500

    Add files via upload

--- demos/demo_offlineTracking.m
+++ demos/demo_offlineTracking.m
@@ -1,3 +1,24 @@
+%===================Script: demo_offineTracking.m==========================
+% 
+% Author: Arun Mahendran Niddish anmahendran@crimson.ua.edu
+%
+% Dependencies: 
+%   +offlineanalysis.rigid_transform_3D 
+%   +offlineanalysis.createMaskCarpetBlue
+%   +offlineanalysis.offinetracker
+%
+% Description: 
+%    To track marker coordinates, get the rotation and translation matrix
+%    in global and body frame for recorded video.
+%
+% Inputs:
+% 1) Number of markers - options.numberofmarkers
+% 2) Which frame to start from - options.startframe
+% 3) Name of the video file to be tracked - inputfilename
+% 4) Name of the tracked video output file - outputfilename
+% 5) Visibility of the plot of output video - options.outputvideo
+%
+% =========================================================================
 clear all;
 clc;    % Clear the command window.
 close all;  % Close all figures (except those of imtool.)
@@ -7,16 +28,9 @@ workspace;  % Make sure the workspace panel is showing.
 format long g;
 format compact;
 
-
-% Initialize
-% Inputs:
-% 1) Number of markers - number_of_markers
-% 2) Which frame to start from - start_frame
-% 3) Name of the video file to be tracked - baseFileName
-
-    inputfilename = 'Marker Focussed Videos\Euler Tours\Euler 7.mp4';  % Video to track
-    outputfilename = 'Phase10_2_gcf';
-    options.startframe = 1;
-    options.numberofmarkers = 8;
-    options.outputvideo = true;
-    tracking_data = offlinetracker(inputfilename,outputfilename,options);
+inputfilename = 'Marker Focussed Videos\Euler Tours\Euler 7.mp4';  % Video to track
+outputfilename = 'Phase10_2_gcf';
+options.startframe = 1;
+options.numberofmarkers = 8;
+options.outputvideo = true;
+tracking_data = offlinetracker(inputfilename,outputfilename,options);
