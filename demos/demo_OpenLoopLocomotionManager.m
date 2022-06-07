657b22424a708f1cd4c8e08c55de59017da650f3
arunniddish <anmahendran@crimson.ua.edu>
2022-06-07 15:27:43 -0500

    Modified open loop with tracking

--- demos/demo_OpenLoopLocomotionManager.m
+++ demos/demo_OpenLoopLocomotionManager.m
@@ -31,9 +31,30 @@ TRAJECTORY_PLAN_MAT = 'data/msoro_openloop_trajectory.mat';
 OUTPUT_MAT_FILE = 'data/msoro_exp_scenario01.mat';
 
 %  MSoRo serial port configuration
-port = '/dev/ttyUSB0';
+port = 'COM4';
 baud = 9600;
 timeout = 30;
+
+
+% Camera Parameters
+cam2 = webcam(2);
+preview(cam2)
+cam2 = camera_properties_marker_1920_Blue(cam2);
+pause(1);
+
+
+
+% Tracker configuration
+numberofmarkers = 8;
+debug_code = 1; % Yes or No (1 or 0)
+ifplot = 1; % Yes or No (1 or 0)
+
+
+onlinetracker_class.numberofmarkers = numberofmarkers;
+PrevPt = zeros(8,3);
+first_pt = zeros(8,3);
+Prev_mean_centroid = zeros(1,3);
+
 
 
 % [2] == Instantiate Matlab-ROS framework for trajectory planning and tracking
